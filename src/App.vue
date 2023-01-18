aa8fc15b33338e0db1a1577aed5927a80a2b6a4a
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-01-18 15:49:03 +0100

    setted the scafolding

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
+  import AppHeader from './components/AppHeader.vue'
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+
+  export default{
+    components:{
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
+  }
 </script>
 
 <template>
- 
+    <header>
+      <AppHeader />
+    </header>
+
+    <main>
+      <AppMain />
+    </main>
+
+    <footer>
+      <AppFooter />
+    </footer>
 </template>
 
-<style>
-
+<style lang="scss">
+  @use './style/general.scss' as*;
+  @use './style/partials/variables' as*;
 </style>
