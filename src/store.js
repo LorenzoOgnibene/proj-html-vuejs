da1e41101d8e7aefe2127c430475c8e1eaccc5e7
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-01-20 12:11:55 +0100

    Change color

--- src/store.js
+++ src/store.js
@@ -121,19 +121,19 @@ export const store = reactive({
             img : 'blog-post-134132600-400x241.jpg',
             title : 'Redeveloping Florida\'s Remote Southern Coast',
             date : 'December 7th 2015',
-            description : 'Technology is Here to Stay Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, quibusdam modi quis nemo dolor voluptates.'
+            description : 'Technology is Here to Stay Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus purus nisl, elementum vitae consequat at, tristique ut enim. Sed ut dignissim leo. Nullam sed metus id sapien faucibus rhoncus sed'
         },
         {
             img : 'blog-post-92486644-400x241.jpg',
             title : 'How We Manage Large Costruction Projects',
             date : 'December 7th 2015',
-            description : 'Technology is Here to Stay Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, quibusdam modi quis nemo dolor voluptates.'
+            description : 'Technology is Here to Stay Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus purus nisl, elementum vitae consequat at, tristique ut enim. Sed ut dignissim leo. Nullam sed metus id sapien faucibus rhoncus sed'
         },
         {
             img : 'blog-post-332773904-400x241.jpg',
             title : 'Future proofing a modern home ',
             date : 'December 6th 2015',
-            description : 'Technology is Here to Stay Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, quibusdam modi quis nemo dolor voluptates.'
+            description : 'Technology is Here to Stay Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus purus nisl, elementum vitae consequat at, tristique ut enim. Sed ut dignissim leo. Nullam sed metus id sapien faucibus rhoncus sed'
         },
     ]
 })
